50a345e972706845c94239f85728618b62abc69f
Rafal Ochyra <aldaron127_dev@o2.pl>
2016-08-16 23:42:02 +0200

    Removal of unnecessary repetition in socket actions

--- shared/User/auth.js
+++ shared/User/auth.js
@@ -1,25 +1,33 @@
+const clientAction = function(name) {
+    return 'auth:client:' + name;
+}
+
+const serverAction = function(name) {
+    return 'auth:server:' + name;
+}
+
 module.exports = {
     SOCKET: {
         NAMESPACE: '/AUTH_NS',
     },
     CLIENT: {
-        REGISTER: 'auth:client:REGISTER',
-        REGISTER_SUCCESS: 'auth:client:REGISTER_SUCCESS',
-        REGISTER_ERROR: 'auth:client:REGISTER_ERROR',
-        LOG_IN: 'auth:client:LOG_IN',
-        LOG_IN_SUCCESS: 'auth:client:LOG_IN_SUCCESS',
-        LOG_IN_ERROR: 'auth:client:LOG_IN_ERROR',
-        LOG_OUT: 'auth:client:LOG_OUT',
-        LOG_OUT_SUCCESS: 'auth:client:LOG_OUT',
+        REGISTER: clientAction('REGISTER'),
+        REGISTER_SUCCESS: clientAction('REGISTER_SUCCESS'),
+        REGISTER_ERROR: clientAction('REGISTER_ERROR'),
+        LOG_IN: clientAction('LOG_IN'),
+        LOG_IN_SUCCESS: clientAction('LOG_IN_SUCCESS'),
+        LOG_IN_ERROR: clientAction('LOG_IN_ERROR'),
+        LOG_OUT: clientAction('LOG_OUT'),
+        LOG_OUT_SUCCESS: clientAction('LOG_OUT'),
     },
     SERVER: {
-        REGISTER: 'auth:server:REGISTER',
-        REGISTER_SUCCESS: 'auth:server:REGISTER_SUCCESS',
-        REGISTER_ERROR: 'auth:server:REGISTER_ERROR',
-        LOG_IN: 'auth:server:LOG_IN',
-        LOG_IN_SUCCESS: 'auth:server:LOG_IN_SUCCESS',
-        LOG_IN_ERROR: 'auth:server:LOG_IN_ERROR',
-        LOG_OUT: 'auth:server:LOG_OUT',
-        LOG_OUT_SUCCESS: 'auth:server:LOG_OUT_SUCCESS',
+        REGISTER: serverAction('REGISTER'),
+        REGISTER_SUCCESS: serverAction('REGISTER_SUCCESS'),
+        REGISTER_ERROR: serverAction('REGISTER_ERROR'),
+        LOG_IN: serverAction('LOG_IN'),
+        LOG_IN_SUCCESS: serverAction('LOG_IN_SUCCESS'),
+        LOG_IN_ERROR: serverAction('LOG_IN_ERROR'),
+        LOG_OUT: serverAction('LOG_OUT'),
+        LOG_OUT_SUCCESS: serverAction('LOG_OUT_SUCCESS'),
     },
 };
