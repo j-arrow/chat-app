500e5d271716b921efaec2e5cbff25e7a549a902
Rafal Ochyra <aldaron127_dev@o2.pl>
2016-08-18 02:19:35 +0200

    Addition of clear search form button

--- client/ui/modules/Friends/components/SearchForm.js
+++ client/ui/modules/Friends/components/SearchForm.js
@@ -7,29 +7,39 @@ class SearchForm extends React.Component {
     constructor(props) {
         super(props);
         this.handleSearch = this.handleSearch.bind(this);
+        this.clear = this.clear.bind(this);
     }
 
     handleSearch(data) {
         this.props.onSearch(data.username);
     }
 
+    clear() {
+        this.form.reset();
+    }
+
     render() {
         return (
             <Formsy.Form
+                ref={form => this.form = form}
                 onSubmit={this.handleSearch}>
                 <div className='row' style={{position:'relative'}}>
-                    <div className='col-xs-10'>
+                    <div className='col-xs-8'>
                         <FormsyText
                             name="username"
                             hintText='Please enter username of your friend'
                             floatingLabelText="Friend's username"
                             fullWidth={true} />
                     </div>
-                    <div className='col-xs-2' style={{position:'absolute',right:0,bottom:0}}>
+                    <div className='col-xs-4' style={{position:'absolute',right:0,bottom:0,textAlign:'right'}}>
                         <FlatButton
                             label='Search'
                             type='submit'
                             primary={true} />
+                        <FlatButton
+                            label='Clear'
+                            secondary={true}
+                            onClick={this.clear} />
                     </div>
                 </div>
             </Formsy.Form>
